d7a24e449c4f4fac8820ce63775fefa89d0fc1e9
Oz <oz@appfigures.com>
2016-05-18 13:51:16 -0400

    Making it work as a commandline tool

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var sh = require('./sh'),
-	findup = require('findup'),
-	path = require('path'),
-	colors = require('colors')
-
-try {
-	var nodeModulesPath = path.resolve(findup.sync(process.cwd(), 'node_modules'), 'node_modules')
-
-	var command = process.argv.slice(2).join(' ')
-
-	console.log(colors.blue('(g)lobal (n)ode_modules') + ' NODE_PATH=' + nodeModulesPath)
-	sh(command, {
-		NODE_PATH: nodeModulesPath
-	})
-} catch (e) {
-	console.error('No node_modules found in the current directory or its ancestors')
-}
+throw new Error('gn is supposed to be used as a commandline tool')
